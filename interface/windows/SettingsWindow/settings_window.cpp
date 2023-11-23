0b35b2f1ceaec944bb8bc3a3aebd0fdd8477484a
yeezy-na-izi <howone1sky@gmail.com>
2023-11-23 22:19:35 +0300

    Some update

--- interface/windows/SettingsWindow/settings_window.cpp
+++ interface/windows/SettingsWindow/settings_window.cpp
@@ -1,5 +1,4 @@
 #include "settings_window.h"
-#include "../../../sources/game.h"
 
 void SettingsWindow::set_text(sf::Text &text, sf::Font &font, std::string &str, int size, sf::Color color,
                               sf::Text::Style style, float x, float y) {
